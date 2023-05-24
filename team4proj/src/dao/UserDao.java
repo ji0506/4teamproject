c700b57fc736da472e259326b9e0d19e0d7bfca2
jiwan <audwldhks0@gmail.com>
2023-05-24 16:10:45 +0900

    자주찾는 사이트 기능 sql 추가

--- team4proj/src/dao/UserDao.java
+++ team4proj/src/dao/UserDao.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import model.User;
+import model.Useraddress;
 
 public class UserDao {
 
@@ -140,4 +141,104 @@ public class UserDao {
 		
 	}
 
+	
+	
+	public boolean createUserAddress(String userId, String rcvrName, String rcvrAddr, String rcvrCp) {
+
+
+		try {
+			Connection conn = SuperDao.getConnection();
+			String sql = "insert into user_address(user_id,rcvr_name,rcvr_addr,rcvr_cp) values(?,?,?,?)";
+			
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, userId);
+			stmt.setString(2, rcvrName);
+			stmt.setString(3, rcvrAddr);
+			stmt.setString(4, rcvrCp);
+
+			stmt.executeUpdate(); // 여기서 에러
+			stmt.close();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean createUserAddress(Useraddress vo) {
+
+
+		try {
+			Connection conn = SuperDao.getConnection();
+			String sql = "insert into user_address(user_id,rcvr_name,rcvr_addr,rcvr_cp) values(?,?,?,?)";
+			
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, vo.getUserId());
+			stmt.setString(2, vo.getRcvrName());
+			stmt.setString(3, vo.getRcvrAddr());
+			stmt.setString(4, vo.getRcvrCp());
+
+			stmt.executeUpdate(); // 여기서 에러
+			stmt.close();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	
+	public Useraddress selectUserAdddress(String userId) {
+		
+		Useraddress vo = null;
+
+		try {
+			Connection conn = SuperDao.getConnection();
+			String sql = "select * from user_address where user_id=?";
+
+			
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, userId);
+			ResultSet re = stmt.executeQuery();
+			while (re.next()) {
+				vo = new Useraddress();
+				vo.setUserId(re.getString("user_id"));
+				vo.setRcvrName(re.getString("rcvr_name"));
+				vo.setRcvrAddr(re.getString("rcvr_addr"));
+				vo.setRcvrCp(re.getString("rcvr_cp"));
+			}
+			re.close();
+			stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		
+
+		return vo;
+	}
+	
+	
+	public void updateAddressCount(Useraddress vo) {
+		try {
+			Connection conn = SuperDao.getConnection();
+			
+
+			String sql = "update user set freq  = ? where user_id=? and rcvr_name=? and rcvr_addr=? and rcvr_cp=? ";
+
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, vo.getFreq());
+			stmt.setString(2, vo.getUserId());
+			stmt.setString(3, vo.getRcvrName());
+			stmt.setString(4, vo.getRcvrAddr());
+			stmt.setString(5, vo.getRcvrCp());
+
+			stmt.executeUpdate();
+			stmt.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
