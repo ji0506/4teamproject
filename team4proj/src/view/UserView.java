e6cc07e6f0cb2deccd59849bb5797d9db79ffce4
KangCheolgu <opopqwqw123@gmail.com>
2023-05-26 12:05:30 +0900

    아이디 중복확인 기능 추가

--- team4proj/src/view/UserView.java
+++ team4proj/src/view/UserView.java
@@ -14,9 +14,9 @@ public class UserView implements CommonView{
 		try {
 			// 로그인창 출력
 			System.out.println("[로그인]");
-			System.out.print("id:");
+			System.out.print("ID\t : ");
 			String userid = scan.nextLine();
-			System.out.print("pwd:");
+			System.out.print("Password : ");
 			String userpwd = scan.nextLine();
 
 			User user = udao.selectById(userid);
@@ -35,7 +35,7 @@ public class UserView implements CommonView{
 			Nonuser nuser = new Nonuser();
 
 			System.out.println("[비회원 로그인]");
-			System.out.print("성함 :");
+			System.out.print("이름 : ");
 			String username = scan.nextLine();
 			System.out.print("전화번호 :");
 			String usercp = scan.nextLine();
@@ -57,17 +57,37 @@ public class UserView implements CommonView{
 
 	public String JoinUser() {
 		try {
-			System.out.println("[회원 가입]");
-			System.out.println("----------------------");
-			System.out.print("ID :");
-			String userid = scan.nextLine();
-			System.out.print("패스워드 :");
+			
+			System.out.println();
+			System.out.println("--------------------------------------------------------");
+			System.out.println();
+			System.out.println("                   ○  회  원  가  입  ○");
+			System.out.println();
+			System.out.println("--------------------------------------------------------");
+			String userid;
+			while (true) {
+				System.out.println("ID를 입력해 주십시오 (15글자 제한)  ");
+				System.out.print("I      D  : ");
+				userid = scan.nextLine();
+				User vo = udao.selectById(userid);
+				
+				if(vo == null) {
+					break;
+				} else {
+					System.out.println("-----------------------");
+					System.out.println("중복된 아이디가 존재합니다.");
+					System.out.println("다시 시도하여 주십시오.");
+					System.out.println("-----------------------");
+					continue;
+				}
+			}
+			System.out.print("Password  : ");
 			String userpw = scan.nextLine();
-			System.out.print("성함 :");
+			System.out.print("성     함  : ");
 			String username = scan.nextLine();
-			System.out.print("전화번호 :");
+			System.out.print("전 화 번 호 : ");
 			String usercp = scan.nextLine();
-			System.out.print("주소 :");
+			System.out.print("주      소 : ");
 			String useraddr = scan.nextLine();
 
 			User user = new User();
